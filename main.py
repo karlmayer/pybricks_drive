45818d27b6b25ee6e366655d6ff4c791dc727314
Karl Mayer <karlmayer@gmail.com>
2022-12-28 22:04:35 -0500

    Formatting

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import curses
 async def display_main(win):
     hub = PybricksHub()
     device = await find_device()
-    # if there are multiple hubs around, you can force a specific hub by setting its BLE address, for example:
+    # if there are multiple hubs around, you can force a specific hub by setting its BLE address:
     # device = await find_device(service="c5f50001-8280-46da-89f4-6d8051e4aeef")
     await hub.connect(device)
     # upload robot.py to hub
@@ -16,8 +16,11 @@ async def display_main(win):
 
     win.clear()
     win.nodelay(True)
-    win.addstr("Connected to Spike Prime Hub, BLE address: " +
-               device.metadata["uuids"][0] + "\n")
+    win.addstr(
+        "Connected to Spike Prime Hub, BLE address: "
+        + device.metadata["uuids"][0]
+        + "\n"
+    )
     win.addstr("Use arrow keys to drive; space to halt car; ESC to exit\n")
 
     while True:
@@ -30,9 +33,9 @@ async def display_main(win):
             await hub.write("U\0".encode("UTF-8"))
         elif key == curses.KEY_DOWN:
             await hub.write("D\0".encode("UTF-8"))
-        elif key == 32: # SPACE
+        elif key == 32:  # SPACE
             await hub.write(" \0".encode("UTF-8"))
-        elif key == 27: # ESC
+        elif key == 27:  # ESC
             await hub.write(b"exit\0")
             sys.exit()
 
